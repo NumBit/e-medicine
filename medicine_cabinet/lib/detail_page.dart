c37a493239bdab04959bea2bc3abac4a5e84201a
Peter Baltazarovič <xbaltaz@fi.muni.cz>
2021-04-13 00:26:16 +0200

    profile

---
+++ medicine_cabinet/lib/detail_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  final String name;
+  const DetailPage({Key key, this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Icon(
+        Icons.medical_services_outlined,
+        color: Color(0xff12263a),
+        size: 50,
+      )),
+    );
+  }
+}
--- medicine_cabinet/lib/widgets/drug_grid_item.dart
+++ medicine_cabinet/lib/widgets/drug_grid_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../detail_page.dart';
+
 class DrugGridItem extends StatelessWidget {
   final String name = "Paralen ultra deluxe";
   final Icon icon = Icon(Icons.medical_services_outlined);
@@ -9,63 +11,81 @@ class DrugGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.grey[300], width: 1),
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey,
-              spreadRadius: 2,
-              blurRadius: 4,
-              offset: Offset(3, 3),
-            )
-          ]),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Material(
+      elevation: 5,
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      child: Stack(
         children: [
           Container(
-              child: Center(
-            child: Icon(
-              Icons.medical_services_outlined,
-              color: Color(0xff12263a),
-              size: 50,
+            padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
             ),
-          )),
-          Container(
-              child: Text(
-            name,
-            textScaleFactor: 1.5,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color(0xff12263a),
-              fontWeight: FontWeight.w400,
-            ),
-          )),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  child: Text(
-                    categories.first,
-                    textScaleFactor: 1.2,
+                    child: Center(
+                  child: Hero(
+                    tag: "icon",
+                    child: Icon(
+                      Icons.medical_services_outlined,
+                      color: Color(0xff12263a),
+                      size: 50,
+                    ),
+                  ),
+                )),
+                Container(
+                    child: Text(
+                  name,
+                  textScaleFactor: 1.5,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xff12263a),
+                    fontWeight: FontWeight.w400,
+                  ),
+                )),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Text(
+                          categories.first,
+                          textScaleFactor: 1.2,
+                        ),
+                      ),
+                      Container(child: getCounterText(count))
+                    ],
                   ),
                 ),
-                Container(
-                  child: Text(
-                    count.toString() + " ks",
-                    textScaleFactor: 1.2,
-                  ),
-                )
               ],
             ),
           ),
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => DetailPage(
+                            name: name,
+                          ))),
+            ),
+          )
         ],
       ),
     );
   }
+}
+
+Widget getCounterText(int count) {
+  return Text(count == 0 ? "Empty" : count.toString() + " ks",
+      textScaleFactor: 1.2,
+      style: TextStyle(
+        color: Color(count <= 3 ? 0xffc33149 : 0xff12263a),
+      ));
 }
