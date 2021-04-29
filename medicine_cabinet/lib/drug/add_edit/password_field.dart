61d622e04b68202c9a01453cac0fb5674e8cbfff
Peter Baltazarovič <43496304+NumBit@users.noreply.github.com>
2021-04-29 17:32:48 +0200

    feat: new login and register page

---
+++ medicine_cabinet/lib/drug/add_edit/password_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PasswordField extends StatelessWidget {
+  const PasswordField({
+    Key key,
+    @required this.label,
+    @required this.controller,
+    this.validator,
+  }) : super(key: key);
+
+  final TextEditingController controller;
+  final String Function(String) validator;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextFormField(
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          maxLengthEnforcement: MaxLengthEnforcement.enforced,
+          decoration: InputDecoration(
+              labelText: label,
+              labelStyle: TextStyle(
+                color: Theme.of(context).primaryColorDark,
+              ),
+              border: OutlineInputBorder()),
+          controller: controller,
+          validator: validator,
+        ));
+  }
+}
---
+++ medicine_cabinet/lib/profile/login_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LoginButton extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  const LoginButton({
+    Key key,
+    this.text,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Theme.of(context).primaryColorDark,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(500.0),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          onPressed: onPressed),
+    );
+  }
+}
