f76aa7ceda3c0ddd06583a1edc0d8fedebc84693
Peter Baltazarovič <43496304+NumBit@users.noreply.github.com>
2021-04-29 18:30:12 +0200

    feat: datepicker

--- medicine_cabinet/lib/drug/detail/add_package.dart
+++ medicine_cabinet/lib/drug/detail/add_package.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:medicine_cabinet/drug/add_edit/custom_form_field.dart';
+import 'package:medicine_cabinet/drug/detail/date_picker_field.dart';
 
 class AddPackage extends StatelessWidget {
   const AddPackage({Key key}) : super(key: key);
@@ -8,36 +10,41 @@ class AddPackage extends StatelessWidget {
   Widget build(BuildContext context) {
     final dosageController = TextEditingController();
     final countController = TextEditingController();
-    DateTime expiration;
+    final timeController = TextEditingController(
+        text: DateFormat('dd.MM.yyyy').format(DateTime.now()));
+    var expiration = DateTime.now();
     return SimpleDialog(
       title: Text("Add package"),
       children: [
-        Form(
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Form(
             child: Column(
-          children: [
-            CustomFormField(label: "Dosage", controller: dosageController),
-            CustomFormField(
-              label: "count",
-              controller: countController,
-              inputType: TextInputType.number,
+              children: [
+                CustomFormField(label: "Dosage", controller: dosageController),
+                CustomFormField(
+                    label: "Count",
+                    controller: countController,
+                    inputType: TextInputType.number),
+                DatePickerField(
+                    label: "Expiration",
+                    controller: timeController,
+                    onTap: (value) {
+                      if (value != null) {
+                        expiration = value;
+                        timeController.text =
+                            DateFormat('dd.MM.yyyy').format(value);
+                      }
+                    }),
+                ElevatedButton(
+                    onPressed: () {
+                      print(expiration);
+                    },
+                    child: Text("Add"))
+              ],
             ),
-            ElevatedButton(
-                onPressed: () {
-                  showDatePicker(
-                          context: context,
-                          initialDate: DateTime.now(),
-                          firstDate: DateTime(2000),
-                          lastDate: DateTime(2500))
-                      .then((value) => expiration = value);
-                },
-                child: Text("Expiration")),
-            ElevatedButton(
-                onPressed: () {
-                  print(expiration);
-                },
-                child: Text("Add"))
-          ],
-        ))
+          ),
+        )
       ],
     );
   }
