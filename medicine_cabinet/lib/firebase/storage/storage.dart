cd8652f5c03a4353fd3da5ef68ab7a2e196abd34
Marius Molcany <molcanym@gmail.com>
2021-05-26 23:32:50 +0200

    feat: storage get link and delete

--- medicine_cabinet/lib/firebase/storage/storage.dart
+++ medicine_cabinet/lib/firebase/storage/storage.dart
@@ -18,7 +18,21 @@ Future<bool> uploadFile(String filePath, String fileTarget) async {
   return true;
 }
 
-Future<bool> downloadFileExample(String filePath, String fileTarget) async {
+Future<String> getLink(String filePath) async {
+  String ref;
+  try {
+    ref = await firebase_storage.FirebaseStorage.instance
+        .ref(filePath)
+        .getDownloadURL();
+  } on firebase_core.FirebaseException catch (e) {
+    snackBarMessage("Download failed", "Please try again later");
+    print("Download failed: " + e.toString());
+    return "https://firebasestorage.googleapis.com/v0/b/e-medicine-3338c.appspot.com/o/empty.jpg?alt=media&token=53f714f3-ebe5-4146-bd07-d0f47a30fcba";
+  }
+  return ref;
+}
+
+Future<bool> downloadFile(String filePath, String fileTarget) async {
   File downloadToFile = File(fileTarget);
   try {
     await firebase_storage.FirebaseStorage.instance
@@ -27,6 +41,17 @@ Future<bool> downloadFileExample(String filePath, String fileTarget) async {
   } on firebase_core.FirebaseException catch (e) {
     snackBarMessage("Download failed", "Please try again later");
     print("Download failed: " + e.toString());
+    return false;
+  }
+  return true;
+}
+
+Future<bool> deleteFile(String filePath) async {
+  try {
+    await firebase_storage.FirebaseStorage.instance.ref(filePath).delete();
+  } on firebase_core.FirebaseException catch (e) {
+    snackBarMessage("Delete failed", "Please try again later");
+    print("Delete failed: " + e.toString());
     return false;
   }
   return true;
