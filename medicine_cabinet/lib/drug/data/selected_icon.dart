3705e9ce0d3a7479ef2c427e083636a838b7f8de
Peter Baltazarovič <43496304+NumBit@users.noreply.github.com>
2021-05-27 03:00:25 +0200

    fix: iconpicker

--- medicine_cabinet/lib/drug/data/selected_icon.dart
+++ medicine_cabinet/lib/drug/data/selected_icon.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SelectedIcon extends GetxController {
-  Rx<IconData?> icon = Icons.mediation_outlined.obs;
+  Rx<IconData?> icon = Icons.medical_services_outlined.obs;
 }
